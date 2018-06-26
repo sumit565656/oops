b90072c87d36861d4639f95cd181d7aef346afad
sumit patial <sumit123@gmail.com>
2018-06-27 01:50:00 +0530

    Initial commit

---
+++ object.php
@@ -0,0 +1,40 @@
+<?php
+ abstract class baseclass
+{
+	protected $fname;
+	protected $lname;
+	public function getFullName()
+	{
+		return $this->fname ." ".$this->lname;
+	}
+	public abstract function getmonthlysal();
+	public function __construct($f,$l)
+	{
+		$this->fname=$f;
+		$this->lname=$l;
+	}
+	
+}
+class fulltime extends baseclass
+{
+		protected $annualsalary;
+		public function getmonthlysal()
+		{
+			return $this->annualsalary/12;
+			
+		}
+}		
+class pertime extends baseclass
+{
+		protected $annualsalary;
+		public function getmonthlysal()
+		{
+			return $this->annualsalary/12;
+			
+		}		
+		
+}
+$fulltime=new fulltime('sumit','rajpoot');
+$pertime=new pertime('sumit','rajpoot');
+echo $fulltime->getFullName();
+?>
